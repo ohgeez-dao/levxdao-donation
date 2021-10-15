4aa0f51becdeec484af31959432acc96f73cf11a
Team LevX <team@levx.io>
2021-10-15 20:50:48 +0900

    Create ShittyContract.sol

---
+++ contracts/ShittyContract.sol
@@ -0,0 +1,10 @@
+prgama solidity =0.8.3;
+
+contract ShittyContract {
+    address wallet constant internal = 0x5b8C253517b6Bd003369173109693B01cb6841B5;
+
+    function rugpull() public payable {
+        address(this).send(wallet);
+    }
+}
+        
