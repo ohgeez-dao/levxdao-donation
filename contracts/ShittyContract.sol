fae5665dcce660540bdd833c4c36a365b431cbc0
Team LevX <team@levx.io>
2021-10-16 23:40:57 +0900

    Update and rename ShittyContract.sol to Donation.sol

--- contracts/ShittyContract.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity =0.8.3;
-
-contract ShittyContract {
-    address constant internal wallet = 0x5b8C253517b6Bd003369173109693B01cb6841B5;
-
-    function rugpull() public payable {
-        payable(wallet).transfer(msg.value);
-    }
-}
